5040a9194b1163c9a12862da142c9650ae2cb95d
Marcos <nodmarcos@gmail.com>
2021-12-15 14:59:21 -0300

    project finished, navigation completed

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,22 @@
+import {Routes, Route} from 'react-router-dom';
+import About from '../../views/examples/About';
+import Home from '../../views/examples/Home';
+import NotFound from '../../views/examples/NotFound';
+import Param from '../../views/examples/Param';
+
+
+import './Content.css';
+
+const Content = props => (
+    <main className="Content">
+        <Routes>
+            <Route path='/about' element={<About/>} />
+            <Route path='/param/:id' element={<Param/>} />
+            <Route exact path='/' element={<Home/>} />
+            <Route path='*' element={<NotFound/>} />
+
+        </Routes>
+    </main>
+)
+
+export default Content;
---
+++ src/views/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router} from 'react-router-dom';
+import './App.css';
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
+
+
+const App = props => (
+    <div className="App">
+        <Router>
+            <Menu />
+            <Content />
+        </Router>
+    </div>
+)
+
+export default App;
